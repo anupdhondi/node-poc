c6a9fc8954e7b9957af7b91f0697d17141f9f20a
anupdhondi5 <anupdhondi5@gmail.com>
2021-01-07 01:43:37 +0530

    Sending user data to Electron

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 9090;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello Electron");
+  res.send({ firstName: "google", lasName: "facebook", isOnCall: true });
 });
 
 app.listen(port, () => {
